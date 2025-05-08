e2836fb89964fa12d69aa40b643bf583c4a98449
EduardoPerezMir <102332110+EduardoPerezMir@users.noreply.github.com>
2025-05-08 18:08:25 -0400

    Archivo de Variación de Velocidades y Tiempos

---
+++ 04_Control_de_Velocidades.ino
@@ -0,0 +1,84 @@
+const int ENA = 10;
+const int IN1 = 9;  
+const int IN2 = 8; 
+
+const int IN4 = 7;
+const int IN3 = 6;
+const int ENB = 5;
+
+void setup() {
+  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT); pinMode(ENB, OUTPUT);
+}
+
+void avanzar(int tiempo, int velocidad) {
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
+  delay(tiempo);
+}
+
+void retroceder(int tiempo, int velocidad) {
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
+  delay(tiempo);
+}
+
+void girarDerecha(int tiempo, int velocidad) {
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
+  delay(tiempo);
+}
+
+void girarIzquierda(int tiempo, int velocidad) {
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
+  analogWrite(ENA, velocidad);
+  analogWrite(ENB, velocidad);
+  delay(tiempo);
+}
+
+void detenerse(int tiempo) {
+  digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+  delay(tiempo);
+}
+
+void loop() {
+  detenerse(4000);
+
+  avanzar(1000, 150);
+  detenerse(2000);
+  
+  avanzar(1000, 200);
+  detenerse(2000);
+
+  retroceder(1000, 255);
+  detenerse(2000);
+
+  avanzar(2000, 150);  
+  detenerse(2000);
+  
+  avanzar(2000, 200);
+  detenerse(2000);
+
+  retroceder(2000, 255);
+  detenerse(2000);
+
+  avanzar(3000, 150);  
+  detenerse(2000);
+  
+  avanzar(3000, 200);
+  detenerse(2000);
+
+  retroceder(3000, 255);
+  detenerse(2000);
+}
